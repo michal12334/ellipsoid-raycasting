db9b5396f407032c4962c3d47d68a90b7f67a7a0
Michal Boguszewski <michal1842001@gmail.com>
2024-02-23 23:55:53 +0100

    Draw new elipsoid when a is updated

--- src/main.rs
+++ src/main.rs
@@ -13,9 +13,6 @@ use crate::pixel::Pixel;
 
 #[derive(Clone, Data, Lens)]
 struct AppState {
-    canvas: Vector<Vector<Pixel>>,
-    width: usize,
-    height: usize,
     a: f64,
     b: f32,
     c: f32,
@@ -23,28 +20,15 @@ struct AppState {
 }
 
 impl AppState {
-    fn new(width: usize, height: usize) -> Self {
-        let canvas = Vector::from((0..height).map(|_| Vector::from(vec![Pixel::new(); width])).collect::<Vec<Vector<Pixel>>>());
-        AppState { canvas, width, height, a: 1.0, b: 1.0, c: 1.0, m: 1, }
-    }
-
-    fn change_pixel_color(&mut self, x: usize, y: usize, color: Color) {
-        self.canvas[y][x] = Pixel::from(color.as_rgba8());
+    fn new() -> Self {
+        AppState { a: 1.0, b: 1.0, c: 1.0, m: 1, }
     }
 }
 
 struct CanvasWidget;
 
 impl Widget<AppState> for CanvasWidget {
-    fn event(&mut self, ctx: &mut EventCtx, event: &Event, data: &mut AppState, _env: &Env) {
-        match event {
-            Event::MouseDown(mouse) => {
-                let (x, y) = (mouse.pos.x as usize, mouse.pos.y as usize);
-                data.change_pixel_color(x, y, Color::BLUE);
-            }
-            _ => (),
-        }
-    }
+    fn event(&mut self, ctx: &mut EventCtx, event: &Event, data: &mut AppState, _env: &Env) {}
 
     fn lifecycle(&mut self, _ctx: &mut LifeCycleCtx, _event: &LifeCycle, _data: &AppState, _env: &Env) {}
 
@@ -57,18 +41,48 @@ impl Widget<AppState> for CanvasWidget {
     }
 
     fn paint(&mut self, ctx: &mut PaintCtx, data: &AppState, _env: &Env) {
+        let rect = ctx.size().to_rect();
+        let width = rect.width() as usize;
+        let height = rect.height() as usize;
+        let mut canvas = Vector::from((0..height).map(|_| Vector::from(vec![Pixel::new(); width])).collect::<Vec<Vector<Pixel>>>());
+
+        for i in 0..width {
+            for j in 0..height {
+                let x = (i as i32 - (width as i32 / 2)) as f32 / ((width / 2) as f32);
+                let y = (j as i32 - (height as i32 / 2)) as f32 / ((height / 2) as f32) * (-1.0);
+
+                let a = data.a as f32;
+                let b = data.b as f32;
+                let c = data.c as f32;
+
+                let l = a * x * x + b * y * y;
+                if l < 1.0 {
+                    let z = (1.0 - l).sqrt() / c;
+
+                    let n = Vector3::new(2.0 * a * x, 2.0 * b * y, 2.0 * c * z).normalize();
+                    let v = Vector3::new(-x, -y, 2.0 - z).normalize();
+
+                    let intensity = n.dot(&v).powi(data.m as i32) as f64;
+
+                    let yellow = Color::YELLOW.as_rgba();
+                    let color = Color::rgb(yellow.0 * intensity, yellow.1 * intensity, yellow.2 * intensity);
+
+                    canvas[j][i] = Pixel::from(color.as_rgba8());
+                }
+            }
+        }
+
         let image = ImageBuf
             ::from_raw(
-                data.canvas.iter()
+                canvas.iter()
                     .flat_map(|f| f)
                     .map(|f| [f.r, f.g, f.b, f.a])
                     .flatten()
                     .collect::<Vec<u8>>(),
                 ImageFormat::RgbaSeparate,
-                data.width,
-                data.height)
+                width,
+                height)
             .to_image(ctx.render_ctx);
-        let rect = ctx.size().to_rect();
         ctx.draw_image(&image, rect, druid::piet::InterpolationMode::Bilinear);
     }
 }
@@ -119,31 +133,7 @@ fn main() -> Result<(), PlatformError> {
         .title(LocalizedString::new("Raycasting"))
         .window_size((width as f64, height as f64));
 
-    let mut initial_state = AppState::new(width, height);
-
-    for i in 0..width {
-        for j in 0..height {
-            let x = (i as i32 - (width as i32 / 2)) as f32 / ((width / 2) as f32);
-            let y = (j as i32 - (height as i32 / 2)) as f32 / ((height / 2) as f32) * (-1.0);
-
-            let a = initial_state.a as f32;
-
-            let l = a * x * x + initial_state.b * y * y;
-            if l < 1.0 {
-                let z = (1.0 - l).sqrt() / initial_state.c;
-
-                let n = Vector3::new(2.0 * a * x, 2.0 * initial_state.b * y, 2.0 * initial_state.c * z).normalize();
-                let v = Vector3::new(-x, -y, 2.0 - z).normalize();
-
-                let intensity = n.dot(&v).powi(initial_state.m as i32) as f64;
-
-                let yellow = Color::YELLOW.as_rgba();
-                let color = Color::rgb(yellow.0 * intensity, yellow.1 * intensity, yellow.2 * intensity);
-
-                initial_state.change_pixel_color(i, j, color);
-            }
-        }
-    }
+    let mut initial_state = AppState::new();
 
     AppLauncher::with_window(main_window)
         .log_to_console()
