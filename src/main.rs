0eb77350dad92936a86757b03b18fe9e9d18c063
Michal Boguszewski <michal1842001@gmail.com>
2024-02-23 23:33:01 +0100

    Set a parameter

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,22 @@
 mod pixel;
 
+use std::fmt::Formatter;
 use druid::widget::prelude::*;
-use druid::{AppLauncher, Color, LocalizedString, PlatformError, Rect, Widget, WindowDesc};
+use druid::{AppLauncher, Color, Lens, LocalizedString, PlatformError, Rect, UnitPoint, Widget, WidgetExt, WindowDesc};
 use druid::piet::{ImageBuf, ImageFormat};
 use druid::platform_menus::mac::file::default;
+use druid::text::ParseFormatter;
+use druid::widget::{BackgroundBrush, Button, Container, Flex, Label, SizedBox, Stepper, TextBox, ValueTextBox};
 use im::Vector;
 use nalgebra::{DMatrix, Matrix, Matrix4, OMatrix, SquareMatrix, Vector3, Vector4};
 use crate::pixel::Pixel;
 
-#[derive(Clone, Data)]
+#[derive(Clone, Data, Lens)]
 struct AppState {
     canvas: Vector<Vector<Pixel>>,
     width: usize,
     height: usize,
-    a: f32,
+    a: f64,
     b: f32,
     c: f32,
     m: u32,
@@ -70,11 +73,49 @@ impl Widget<AppState> for CanvasWidget {
     }
 }
 
+fn built_ui() -> impl Widget<AppState> {
+    Flex::row()
+        .with_flex_child(CanvasWidget { }.expand(), 5.0)
+        .with_flex_child(
+            Flex::column()
+                .with_flex_child(
+                    Flex::row()
+                        .with_flex_child(
+                            Label::new("a:")
+                                .align_vertical(UnitPoint::TOP_LEFT)
+                                .expand_width(),
+                            1.0
+                        )
+                        .with_flex_child(
+                            TextBox::new()
+                                .with_formatter(ParseFormatter::new())
+                                .lens(AppState::a)
+                                .align_vertical(UnitPoint::TOP)
+                                .expand_width(),
+                            5.0
+                        )
+                        .with_flex_child(
+                            Stepper::new()
+                                .with_range(0.1, 10.0)
+                                .with_step(0.1)
+                                .lens(AppState::a)
+                                .align_vertical(UnitPoint::TOP_RIGHT)
+                                .expand_width(),
+                            1.0
+                        )
+                        .expand_width(),
+                    1.0
+                )
+                .expand(),
+            1.0
+        )
+}
+
 fn main() -> Result<(), PlatformError> {
     let width = 800usize;
     let height = 600usize;
 
-    let main_window = WindowDesc::new(CanvasWidget { })
+    let main_window = WindowDesc::new(built_ui())
         .title(LocalizedString::new("Raycasting"))
         .window_size((width as f64, height as f64));
 
@@ -85,11 +126,13 @@ fn main() -> Result<(), PlatformError> {
             let x = (i as i32 - (width as i32 / 2)) as f32 / ((width / 2) as f32);
             let y = (j as i32 - (height as i32 / 2)) as f32 / ((height / 2) as f32) * (-1.0);
 
-            let l = initial_state.a * x * x + initial_state.b * y * y;
+            let a = initial_state.a as f32;
+
+            let l = a * x * x + initial_state.b * y * y;
             if l < 1.0 {
                 let z = (1.0 - l).sqrt() / initial_state.c;
 
-                let n = Vector3::new(2.0 * initial_state.a * x, 2.0 * initial_state.b * y, 2.0 * initial_state.c * z).normalize();
+                let n = Vector3::new(2.0 * a * x, 2.0 * initial_state.b * y, 2.0 * initial_state.c * z).normalize();
                 let v = Vector3::new(-x, -y, 2.0 - z).normalize();
 
                 let intensity = n.dot(&v).powi(initial_state.m as i32) as f64;
