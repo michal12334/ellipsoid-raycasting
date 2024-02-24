bb8bfec2c6495962b4cfe1d72e756ee63d5766bd
Michal Boguszewski <michal1842001@gmail.com>
2024-02-24 13:12:13 +0100

    Add ambient

--- src/main.rs
+++ src/main.rs
@@ -63,7 +63,7 @@ impl Widget<AppState> for CanvasWidget {
                     let n = Vector3::new(2.0 * a * x, 2.0 * b * y, 2.0 * c * z).normalize();
                     let v = Vector3::new(-x, -y, 100.0 - z).normalize();
 
-                    let intensity = n.dot(&v).powi(m) as f64;
+                    let intensity = (n.dot(&v).powi(m) as f64 + 0.1).clamp(0.0, 1.0);
 
                     let yellow = Color::YELLOW.as_rgba();
                     let color = Color::rgb(yellow.0 * intensity, yellow.1 * intensity, yellow.2 * intensity);
