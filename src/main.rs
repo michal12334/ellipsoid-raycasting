faed6cc4b0f8619d5a76d45fdf22bb506d485c4b
Michal Boguszewski <michal1842001@gmail.com>
2024-02-24 13:29:05 +0100

    Move drawing logic to canvas and limit memory allocation

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod pixel;
 
 use std::fmt::Formatter;
+use std::vec;
 use druid::widget::prelude::*;
 use druid::{AppLauncher, Color, Lens, lens, LocalizedString, PlatformError, Rect, UnitPoint, Widget, WidgetExt, WindowDesc};
 use druid::piet::{ImageBuf, ImageFormat};
@@ -25,7 +26,80 @@ impl AppState {
     }
 }
 
-struct CanvasWidget;
+struct CanvasWidget {
+    canvas: Vec<u8>,
+    a: f64,
+    b: f64,
+    c: f64,
+    m: f64,
+    width: usize,
+    height: usize,
+}
+
+impl CanvasWidget {
+    fn new() -> Self {
+        CanvasWidget {
+            canvas: Vec::new(),
+            a: 1.0,
+            b: 1.0,
+            c: 1.0,
+            m: 1.0,
+            width: 0,
+            height: 0,
+        }
+    }
+    
+    fn draw(&mut self, a: f64, b: f64, c: f64, m: f64, width: usize, height: usize) {
+        if !self.update(a, b, c, m, width, height) {
+            return;
+        }
+        
+        self.canvas.resize(width * height * 4, 0);
+
+        let a = a as f32;
+        let b = b as f32;
+        let c = c as f32;
+        let m = m as i32;
+
+        for i in 0..width {
+            for j in 0..height {
+                let x = (i as i32 - (width as i32 / 2)) as f32 / ((width / 2) as f32);
+                let y = (j as i32 - (height as i32 / 2)) as f32 / ((height / 2) as f32) * (-1.0);
+                let index = (j * width + i) * 4;
+
+                let l = a * x * x + b * y * y;
+                if l < 1.0 {
+                    let z = (1.0 - l).sqrt() / c;
+
+                    let n = Vector3::new(2.0 * a * x, 2.0 * b * y, 2.0 * c * z).normalize();
+                    let v = Vector3::new(-x, -y, 100.0 - z).normalize();
+
+                    let intensity = (n.dot(&v).powi(m) as f64 + 0.1).clamp(0.0, 1.0);
+
+                    let yellow = Color::YELLOW.as_rgba();
+                    let color = Color::rgb(yellow.0 * intensity, yellow.1 * intensity, yellow.2 * intensity);
+                    
+                    (self.canvas[index], self.canvas[index + 1], self.canvas[index + 2], self.canvas[index + 3]) = color.as_rgba8(); 
+                } else {
+                    (self.canvas[index], self.canvas[index + 1], self.canvas[index + 2], self.canvas[index + 3]) = (0, 0, 0, 255);
+                }
+            }
+        }
+    }
+    
+    fn update(&mut self, a: f64, b: f64, c: f64, m: f64, width: usize, height: usize) -> bool {
+        let result = self.a != a || self.b != b || self.c != c || self.m != m || self.width != width || self.height != height;
+        
+        self.a = a;
+        self.b = b;
+        self.c = c;
+        self.m = m;
+        self.width = width;
+        self.height = height;
+        
+        result
+    }
+}
 
 impl Widget<AppState> for CanvasWidget {
     fn event(&mut self, ctx: &mut EventCtx, event: &Event, data: &mut AppState, _env: &Env) {}
@@ -44,45 +118,16 @@ impl Widget<AppState> for CanvasWidget {
         let rect = ctx.size().to_rect();
         let width = rect.width() as usize;
         let height = rect.height() as usize;
-        let mut canvas = Vector::from((0..height).map(|_| Vector::from(vec![Pixel::new(); width])).collect::<Vec<Vector<Pixel>>>());
-
-        for i in 0..width {
-            for j in 0..height {
-                let x = (i as i32 - (width as i32 / 2)) as f32 / ((width / 2) as f32);
-                let y = (j as i32 - (height as i32 / 2)) as f32 / ((height / 2) as f32) * (-1.0);
-
-                let a = data.a as f32;
-                let b = data.b as f32;
-                let c = data.c as f32;
-                let m = data.m as i32;
-
-                let l = a * x * x + b * y * y;
-                if l < 1.0 {
-                    let z = (1.0 - l).sqrt() / c;
-
-                    let n = Vector3::new(2.0 * a * x, 2.0 * b * y, 2.0 * c * z).normalize();
-                    let v = Vector3::new(-x, -y, 100.0 - z).normalize();
-
-                    let intensity = (n.dot(&v).powi(m) as f64 + 0.1).clamp(0.0, 1.0);
-
-                    let yellow = Color::YELLOW.as_rgba();
-                    let color = Color::rgb(yellow.0 * intensity, yellow.1 * intensity, yellow.2 * intensity);
-
-                    canvas[j][i] = Pixel::from(color.as_rgba8());
-                }
-            }
-        }
+        
+        self.draw(data.a, data.b, data.c, data.m, width, height);
 
         let image = ImageBuf
             ::from_raw(
-                canvas.iter()
-                    .flat_map(|f| f)
-                    .map(|f| [f.r, f.g, f.b, f.a])
-                    .flatten()
-                    .collect::<Vec<u8>>(),
+                self.canvas.clone(),
                 ImageFormat::RgbaSeparate,
                 width,
-                height)
+                height
+            )
             .to_image(ctx.render_ctx);
         ctx.draw_image(&image, rect, druid::piet::InterpolationMode::Bilinear);
     }
@@ -90,7 +135,7 @@ impl Widget<AppState> for CanvasWidget {
 
 fn build_ui() -> impl Widget<AppState> {
     Flex::row()
-        .with_flex_child(CanvasWidget { }.expand(), 5.0)
+        .with_flex_child(CanvasWidget::new().expand(), 5.0)
         .with_flex_child(
             Flex::column()
                 .with_flex_child(
